8fbfc6e203dc9b94135dbb15c654149b84f4d4af
Aierse <dbsgur2008@gmail.com>
2022-11-17 01:49:33 +0900

    update - Dict update

--- TypeScript/nomadcoder/CLASSES AND INTERFACES/4-1.ts
+++ TypeScript/nomadcoder/CLASSES AND INTERFACES/4-1.ts
@@ -9,13 +9,26 @@ class Dict {
     }
 
     add(word: Word) {
-        if(this.words[word.term] === undefined){
+        if (this.words[word.term] === undefined) {
             this.words[word.term] = word.def
         }
     }
 
-    def(term:string) {
+    def(term: string) {
         return this.words[term]
+    }
+    // Code Challenge
+    // update Overload Signature
+    update(key: Word): void
+    update(term:string, def: string): void
+
+    update(key: string | Word, def?: string): void {
+        if (key instanceof Word) {
+            this.words[key.term] = key.def
+        } 
+        else if (def) {
+            this.words[key] = def
+        }
     }
 }
 
@@ -23,11 +36,18 @@ class Word {
     constructor(
         public term: string,
         public def: string
-    ) {}
+    ) { }
 }
 
 const kimchi4_1 = new Word("kimchi", "한국의 음식")
 
 const dict = new Dict()
+
 dict.add(kimchi4_1)
 console.log(dict.def("kimchi"))
+
+dict.update("kimchi", "string overload")
+console.log(dict.def('kimchi'))
+
+dict.update(new Word('kimchi', "word overload"))
+console.log(dict.def('kimchi'))
