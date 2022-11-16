ea676aa09422ca262c62f3600c44e5f696772cab
Aierse <dbsgur2008@gmail.com>
2022-11-17 00:58:56 +0900

    Update - allow abstaract

--- TypeScript/nomadcoder/CLASSES AND INTERFACES/4-0.ts
+++ TypeScript/nomadcoder/CLASSES AND INTERFACES/4-0.ts
@@ -1,11 +1,20 @@
-class Player4_0 {
-    constructor (
-        private firstname:string,
-        private lastname:string,
-        public nickname:string
-    ){}
+abstract class User {
+    constructor(
+        private firstname: string,
+        private lastname: string,
+        public nickname: string
+    ) { }
+
+    getFullName() {
+        return `${this.firstname} ${this.lastname}`
+    }
+}
+
+class Player4_0 extends User {
+
 }
 
 const jo4_0 = new Player4_0("조", "윤혁", "Aierse")
 
 console.log(jo4_0.nickname)
+console.log(jo4_0.getFullName)
