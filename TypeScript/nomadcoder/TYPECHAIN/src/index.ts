ef93d263136e372cf52819e356f2591aeddf75a9
Aierse <dbsgur2008@gmail.com>
2022-11-17 21:02:13 +0900

    Update - BlockChain class

--- TypeScript/nomadcoder/TYPECHAIN/src/index.ts
+++ TypeScript/nomadcoder/TYPECHAIN/src/index.ts
@@ -14,12 +14,28 @@ class Block implements BlockShape {
         public prevHash: string,
         public height: number,
         public data: string
-    ) { 
+    ) {
         this.hash = Block.calculateHash(prevHash, height, data)
     }
 
-    static calculateHash(prevHash: string, height:number, data: string) {
+    static calculateHash(prevHash: string, height: number, data: string) {
         const toHash = `${prevHash}${height}${data}`
         return crypto.createHash("sha256").update(toHash).digest("hex")
+    }
+}
+
+class BlockChain {
+    private blocks: Block[]
+    constructor() {
+        this.blocks = []
+    }
+
+    private getPrevHash() {
+        if (this.blocks.length === 0) return ""
+        return this.blocks[this.blocks.length - 1].hash
+    }
+
+    public addBlock(data: string) {
+        const block = new Block(this.getPrevHash(), this.blocks.length + 1, data)
     }
 }
