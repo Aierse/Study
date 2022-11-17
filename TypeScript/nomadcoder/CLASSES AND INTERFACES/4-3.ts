209b0439a172a62d88341837aa319a914fb612a0
Aierse <dbsgur2008@gmail.com>
2022-11-17 13:35:31 +0900

    Update - Player class

--- TypeScript/nomadcoder/CLASSES AND INTERFACES/4-3.ts
+++ TypeScript/nomadcoder/CLASSES AND INTERFACES/4-3.ts
@@ -7,3 +7,13 @@ abstract class User4_3 {
     abstract sayHi(name: string): string
     abstract fullName(): string
 }
+
+class Player4_3 extends User4_3 {
+    fullName(): string {
+        return `${this.firstname} ${this.lastname}`
+    }
+
+    sayHi(name: string): string {
+        return `Hello ${name}. My name is ${this.fullName()}`
+    }
+}
