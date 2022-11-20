c144bc4e71a92392d481086de4c46a5dea66a69c
Aierse <dbsgur2008@gmail.com>
2022-11-20 21:07:08 +0900

    Update movies.controller.ts

--- TypeScript/nomadcoder/NestJS/hi-nest/src/movies/movies.controller.ts
+++ TypeScript/nomadcoder/NestJS/hi-nest/src/movies/movies.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 
 @Controller('movies')
 export class MoviesController {
@@ -7,14 +7,20 @@ export class MoviesController {
         return 'This will return all movies'
     }
 
+    @Get('search')
+    search(@Query('year') searchingYear: string) {
+        return `We are searching for a movie title`
+    }
+
     @Get('/:id')
     getOne(@Param('id') moiveId: string) {
         return `This will return one movie with the id : ${moiveId}`
     }
 
     @Post()
-    create() {
-        return 'This will create a movie'
+    create(@Body() movieData) {
+        console.log(movieData)
+        return movieData
     }
 
     @Delete('/:id')
@@ -23,7 +29,10 @@ export class MoviesController {
     }
 
     @Patch('/:id')
-    patch(@Param('id') movieId: string) {
-        return `This will patch a movie with the id : ${movieId}`
+    patch(@Param('id') movieId: string, @Body() updateData) {
+        return {
+            updatedMovie: movieId,
+            ...updateData
+        }
     }
 }
