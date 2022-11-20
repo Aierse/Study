357ca94b90a4bc73c7be7e3e81378474b1d15f02
Aierse <dbsgur2008@gmail.com>
2022-11-20 14:57:18 +0900

    Update - Architecture

--- TypeScript/nomadcoder/NestJS/hi-nest/src/app.controller.ts
+++ TypeScript/nomadcoder/NestJS/hi-nest/src/app.controller.ts
@@ -7,11 +7,11 @@ export class AppController {
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return this.appService.getHello()
   }
 
   @Get('/hello')
   sayHello(): string {
-    return 'hello everyone'
+    return this.appService.sayHello()
   }
 }
