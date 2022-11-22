d0c3d551ef28f279734516081adccf72e1f24b98
Aierse <dbsgur2008@gmail.com>
2022-11-22 17:19:25 +0900

    update - allow movieDTO validation

--- TypeScript/nomadcoder/NestJS/hi-nest/src/movies/dto/create-movie.dto.ts
+++ TypeScript/nomadcoder/NestJS/hi-nest/src/movies/dto/create-movie.dto.ts
@@ -1,5 +1,10 @@
+import { IsString, IsNumber } from "class-validator";
+
 export class CreateMovieDto {
-    readonly year: number;
+    @IsString()
     readonly title: string;
+    @IsNumber()
+    readonly year: number;
+    @IsString({ each: true })
     readonly genres: string[];
 }
