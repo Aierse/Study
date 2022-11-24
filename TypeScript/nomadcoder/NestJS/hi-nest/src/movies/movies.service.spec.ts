91db0a55295c0aeea441d214dac637803a82d953
Aierse <dbsgur2008@gmail.com>
2022-11-24 15:48:55 +0900

    update - add 404 error deleteOne test case

--- TypeScript/nomadcoder/NestJS/hi-nest/src/movies/movies.service.spec.ts
+++ TypeScript/nomadcoder/NestJS/hi-nest/src/movies/movies.service.spec.ts
@@ -60,5 +60,13 @@ describe('MoviesService', () => {
       const afterDelete = service.getAll().length
       expect(afterDelete).toBeLessThan(beforeDelete)
     })
+
+    it('should throw 404 error', () => {
+      try {
+        service.deleteOne(999)
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException)
+      }
+    })
   })
 });
