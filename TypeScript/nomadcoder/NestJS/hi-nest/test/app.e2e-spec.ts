95dcce3cb72ff5e4b52ee63b93f60bbaefbdd581
Aierse <dbsgur2008@gmail.com>
2022-11-25 22:08:42 +0900

    create - movies:/id patch 200 testcase

--- TypeScript/nomadcoder/NestJS/hi-nest/test/app.e2e-spec.ts
+++ TypeScript/nomadcoder/NestJS/hi-nest/test/app.e2e-spec.ts
@@ -63,5 +63,11 @@ describe('AppController (e2e)', () => {
       return request(app.getHttpServer()).get('/movies/999')
       .expect(404)
     })
+
+    it('PATCH 200', () => {
+      return request(app.getHttpServer()).patch('/movies/1')
+      .send({title: 'Updated Test'})
+      .expect(200)
+    })
   })
 });
