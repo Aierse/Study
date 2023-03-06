9253dbc356790b05ce1549156ebff913f4384189
Aierse <dbsgur2008@gmail.com>
2023-03-06 23:31:07 +0900

    learn: cleanup
    컴포넌트가 destroy될 때 실행시키고 싶은 코드가 있다면 useEffect에서 function을 리턴해준다.

--- TypeScript/nomadcoder/ReactJS/react-for-beginners/src/App.js
+++ TypeScript/nomadcoder/ReactJS/react-for-beginners/src/App.js
@@ -1,33 +1,21 @@
 import { useEffect, useState } from "react";
 
+function Hello() {
+  useEffect(() => {
+    console.log("created");
+    return () => console.log("destroyed");
+  });
+  return <h1>Hello</h1>;
+}
+
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState();
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
 
-  const onChange = (event) => setKeyword(event.target.value);
-  const onClick = () => setValue((prev) => prev + 1);
-
-  console.log("i run all the time");
-  useEffect(() => console.log("i run only once"), []);
-  useEffect(() => {
-    console.log("i run when 'keyword' changes.");
-  }, [keyword]);
-  useEffect(() => {
-    console.log("i run when 'counter' changes.");
-  }, [counter]);
-  useEffect(() => {
-    console.log("i run when 'keyword & counter' changes.");
-  }, [keyword, counter]);
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search here..."
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
