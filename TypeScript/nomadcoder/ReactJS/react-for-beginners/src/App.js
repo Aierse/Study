3bd3dce168b11e954c7f5624fad40784ad4ab5c4
Aierse <dbsgur2008@gmail.com>
2023-03-08 00:05:05 +0900

    learn: ToDo 리스트 만들기

--- TypeScript/nomadcoder/ReactJS/react-for-beginners/src/App.js
+++ TypeScript/nomadcoder/ReactJS/react-for-beginners/src/App.js
@@ -1,21 +1,34 @@
 import { useEffect, useState } from "react";
 
-function Hello() {
-  useEffect(() => {
-    console.log("created");
-    return () => console.log("destroyed");
-  });
-  return <h1>Hello</h1>;
-}
-
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [toDo, setTodo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setTodo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") return;
 
+    setTodo("");
+    setToDos((cur) => [toDo, ...toDos]);
+  };
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
